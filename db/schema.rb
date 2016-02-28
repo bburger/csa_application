29993fda9f5434ff3dce69bb55b8613b39f09ad2
Ben Burger <benburger10@gmail.com>
2016-02-28 10:44:56 -0500

    created line_items, order scaffolds. Cart controller with add_to_cart, view_order and checkout views. associated line_items, orders, and users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160228145717) do
+ActiveRecord::Schema.define(version: 20160228153333) do
 
   create_table "boxes", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,25 @@ ActiveRecord::Schema.define(version: 20160228145717) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "line_items", force: :cascade do |t|
+    t.integer  "box_id"
+    t.integer  "quantity"
+    t.decimal  "line_item_total"
+    t.integer  "order_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.decimal  "subtotal"
+    t.decimal  "sales_tax"
+    t.decimal  "grand_total"
+    t.integer  "user_id"
+    t.text     "order_items"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "users", force: :cascade do |t|
