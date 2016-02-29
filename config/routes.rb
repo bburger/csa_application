9e69b67f5774a647120089e2e4c93bdc53c47a3b
Ben Burger <benburger10@gmail.com>
2016-02-29 09:53:05 -0500

    added styling from Greta

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'farms#index'
+  root to: 'farms#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
